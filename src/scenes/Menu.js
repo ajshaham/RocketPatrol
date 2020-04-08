c3128c6011491c24bab2f28c829710aa6043cea9
Jewce <ashaham@ucsc.edu>
2020-04-08 15:47:23 -0700

    Scenes & assets implemented.

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,5 +1,14 @@
-class Menu extends Phaser.Scene{
+class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
+    }
+
+    create() {
+        console.log(this);
+        // Display menu text
+        this.add.text(20, 20, "Rocket Patrol Menu");
+
+        // Launch next scene
+        this.scene.start("playScene");
     }
 }
